dfdae33afcad197ce4d9bf3d293aaf77fdf39726
Dyke <dykekangoma@gmail.com>
2024-07-17 08:07:31 +0200

    Dive into Python Magic Methods

--- oop/book_class.py
+++ oop/book_class.py
@@ -12,6 +12,9 @@ class Book:
     def __str__(self):
         return f"'{self.title}' by {self.author} (published in {self.year})"
     
+    def __del__(self):
+        return f"Deleting({self.year})"
+    
 
     def __eq__(self, other):
         if isinstance(other, Book):
