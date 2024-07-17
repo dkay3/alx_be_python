29c7ee2753a6e7c25deee7f10b22150f9f26427d
Dyke <dykekangoma@gmail.com>
2024-07-17 08:57:54 +0200

    Mastering Inheritance and Composition in Python

---
+++ oop/library_system.py
@@ -0,0 +1,34 @@
+class Book:
+    def __init__(self, title, author):
+        self.title = title
+        self.author = author
+
+    def __str__(self):
+        return f"{self.title} by {self.author}"
+
+class EBook(Book):
+    def __init__(self, title, author, file_size):
+        super().__init__(title, author)
+        self.file_size = file_size
+
+    def __str__(self):
+        return super().__str__() + f" (File Size:  {self.file_size}KB)"
+
+class PrintBook(Book):
+    def __init__(self, title, author, pages):
+        super().__init__(title, author)
+        self.pages = pages
+
+    def __str__(self):
+        return super().__str__() + f" (Page Count: {self.pages} )"
+
+class Library:
+    def __init__(self):
+        self.collection = []
+
+    def add_book(self, book):
+        self.collection.append(book)
+
+    def list_books(self):
+        for book in self.collection:
+            print(book)
